f17b5d5c70d70e647b65acd0a6e381eafb78c33c
WSTxda <wstxda@gmail.com>
2025-01-06 15:13:34 -0300

    Update module action

--- module/action.sh
+++ module/action.sh
@@ -5,7 +5,7 @@ APP_PACKAGE="com.wstxda.viper4android"
 APP_ACTIVITY="com.wstxda.viper4android.MainActivity"
 DOWNLOAD_URL="https://github.com/WSTxda/ViperFX-RE-Releases/releases/latest"
 
-# Action button messages
+# Module action messages
 log_message() {
     echo "[INFO] $1"
 }
@@ -15,10 +15,11 @@ error_message() {
 
 # Start ViPER4Android FX Redesign application
 log_message "Opening application..."
-if am start -n "$APP_PACKAGE/$APP_ACTIVITY" > /dev/null 2>&1; then
+if pm list packages | grep -q "$APP_PACKAGE"; then
+    am start -n "$APP_PACKAGE/$APP_ACTIVITY"
     log_message "ViperFX opened successfully!"
 else
     error_message "Failed to open ViperFX. Activity not found."
     log_message "App not installed, download and install the latest version from GitHub repository:"
-    echo "$DOWNLOAD_URL"
+    am start -a android.intent.action.VIEW -d "$DOWNLOAD_URL"
 fi

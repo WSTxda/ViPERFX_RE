45d74493e5c06e8b5d79e7d686d2b52e0e91678d
Marcos Raphael <mraphael.py@gmail.com>
2026-02-25 12:04:37 -0300

    add ksu support

--- module/common/install.sh
+++ module/common/install.sh
@@ -8,22 +8,28 @@ cp_ch -n $MODPATH/common/files/libv4a_re_$ABI.so $MODPATH$LIBDIR/lib64/soundfx/l
 fi
 
 ui_print "    Patching audio_effects config files"
-CFGS="$(find /odm /system /vendor -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml")"
+CFGS="$(find /odm /system /vendor -type f \( -name "*audio_effects*.conf" -o -name "*audio_effects*.xml" \))"
 for OFILE in ${CFGS}; do
   FILE="$MODPATH$(echo $OFILE | sed "s|^/vendor|/system/vendor|g")"
   cp_ch -n $OFILE $FILE
   case $FILE in
     *.conf)
-        sed -i "/v4a_standard_re {/,/}/d" $FILE
-        sed -i "/v4a_re {/,/}/d" $FILE
-        sed -i "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" $FILE
-        sed -i "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" $FILE
+        sed -i \
+          -e "/v4a_standard_re {/,/}/d" \
+          -e "/v4a_re {/,/}/d" \
+          "$FILE"
+        sed -i \
+          -e "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" \
+          -e "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" \
+          "$FILE"
         ;;
-    *.xml) 
-        sed -i "/v4a_standard_re/d" $FILE
-        sed -i "/v4a_re/d" $FILE
-        sed -i "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" $FILE
-        sed -i "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" $FILE
+    *.xml)
+        sed -i \
+          -e "/v4a_standard_re/d" \
+          -e "/v4a_re/d" \
+          -e "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" \
+          -e "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" \
+          "$FILE"
         ;;
   esac
 done
--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -1,23 +1,76 @@
-LIBPATCH=`cat $MODPATH/libpatch.txt`
-CFGS="$(find /odm /system /vendor -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml")"
+LIBPATCH=$(cat "$MODPATH/libpatch.txt")
+CFGS="$(find /odm /system /vendor -type f \( -name "*audio_effects*.conf" -o -name "*audio_effects*.xml" \))"
 for FILE in ${CFGS}; do
   case $FILE in
     *.conf)
-        sed -i "/v4a_standard_re {/,/}/d" $FILE
-        sed -i "/v4a_re {/,/}/d" $FILE
-        sed -i "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" $FILE
-        sed -i "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" $FILE
+        sed -i \
+          -e "/v4a_standard_re {/,/}/d" \
+          -e "/v4a_re {/,/}/d" \
+          "$FILE"
+        sed -i \
+          -e "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" \
+          -e "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" \
+          "$FILE"
         ;;
-    *.xml) 
-        sed -i "/v4a_standard_re/d" $FILE
-        sed -i "/v4a_re/d" $FILE
-        sed -i "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" $FILE
-        sed -i "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" $FILE
+    *.xml)
+        sed -i \
+          -e "/v4a_standard_re/d" \
+          -e "/v4a_re/d" \
+          -e "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" \
+          -e "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" \
+          "$FILE"
         ;;
   esac
 done
 
-if [ -d "/odm/etc/" ]; then
-  echo "Binding audio_effects.xml to odm partition..."
-  mount -o bind /data/adb/modules/ViPER4Android-RE/odm/etc/audio_effects.xml /odm/etc/audio_effects.xml
+if [ -d "/odm/etc/" ] && [ -f "$MODPATH/odm/etc/audio_effects.xml" ]; then
+  if ! mountpoint -q /odm/etc/audio_effects.xml 2>/dev/null; then
+    echo "Binding audio_effects.xml to odm partition..."
+    mount --bind "$MODPATH/odm/etc/audio_effects.xml" /odm/etc/audio_effects.xml
+  fi
+fi
+
+
+if [ ! -d "/data/adb/magisk" ]; then
+  echo "[ViPER] Raiz não-Magisk detectada (KernelSU/APatch), aplicando bind mounts manuais..."
+
+  LIBDIR_REAL=$(echo "$LIBPATCH" | sed 's/\\\//\//g')
+
+  for BITNESS in lib lib64; do
+    SRC="$MODPATH/system/vendor/${BITNESS}/soundfx/libv4a_re.so"
+    [ ! -f "$SRC" ] && SRC="$MODPATH/system/${BITNESS}/soundfx/libv4a_re.so"
+
+    DST="${LIBDIR_REAL}/${BITNESS}/soundfx/libv4a_re.so"
+    DSTDIR="${LIBDIR_REAL}/${BITNESS}/soundfx"
+
+    if [ -f "$SRC" ]; then
+      if [ ! -d "$DSTDIR" ]; then
+        echo "[ViPER] Criando $DSTDIR"
+        mkdir -p "$DSTDIR"
+        chcon --reference="${LIBDIR_REAL}/${BITNESS}" "$DSTDIR" 2>/dev/null || true
+      fi
+
+      if [ ! -f "$DST" ]; then
+        echo "[ViPER] Copiando $SRC para $DST"
+        cp "$SRC" "$DST"
+        chmod 644 "$DST"
+        restorecon "$DST" 2>/dev/null || chcon u:object_r:vendor_file:s0 "$DST" 2>/dev/null || true
+      fi
+
+      if ! mountpoint -q "$DST" 2>/dev/null; then
+        mount --bind "$SRC" "$DST" \
+          && echo "[ViPER] Bind mount aplicado: $DST" \
+          || echo "[ViPER] Falha no bind mount: $DST"
+      fi
+    fi
+  done
+
+  for CFG_FILE in ${CFGS}; do
+    MOD_CFG="$MODPATH$(echo "$CFG_FILE" | sed 's|^/vendor|/system/vendor|g')"
+    if [ -f "$MOD_CFG" ] && ! mountpoint -q "$CFG_FILE" 2>/dev/null; then
+      mount --bind "$MOD_CFG" "$CFG_FILE" \
+        && echo "[ViPER] Config bind mount: $CFG_FILE" \
+        || echo "[ViPER] Falha config bind mount: $CFG_FILE"
+    fi
+  done
 fi
